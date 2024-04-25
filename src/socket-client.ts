96c1da9cb1f2591a49e8ad328752d587c829cfed
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-04-25 08:49:09 -0400

    fix: modify url socket

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -3,12 +3,15 @@ import { Manager, Socket } from 'socket.io-client';
 let socket: Socket;
 
 export const connectToServer = (token: string) => {
-  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
-    extraHeaders: {
-      hola: 'mundo',
-      authentication: token,
-    },
-  });
+  const manager = new Manager(
+    'https://technicalshop-production.up.railway.app/socket.io/socket.io.js',
+    {
+      extraHeaders: {
+        hola: 'mundo',
+        authentication: token,
+      },
+    }
+  );
 
   socket?.removeAllListeners();
   socket = manager.socket('/');
